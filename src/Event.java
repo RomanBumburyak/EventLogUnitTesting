a0919f1c00e7dbd1728156ef74fed5828bac8244
RomanBumburyak <romanbumburyak@gmail.com>
2017-09-21 16:00:44 -0400

    Second commit working on the Tests

--- src/Event.java
+++ src/Event.java
@@ -1,6 +1,7 @@
 public class Main {
     public static void main(String[] args) {
 
+        System.out.println("Working on my EventLog Class C");
 
 
 
@@ -16,6 +17,6 @@ public class Main {
 
 
 
-        
+
     }
 }
